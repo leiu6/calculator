11cddf92f6c35800e5d4c5fc44f1a2c2e1831bc0
Leighton Mitchell <ltmitchell2000@gmail.com>
2021-04-25 16:24:01 -0500

    Added some rudimentary arithmetic operations such as add, subtract, divide, and multiply. Still needs more refactoring and editing to make usable

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -3,6 +3,7 @@ const display = document.querySelector(".displayArea");
 let currentNumber = new String();
 let altNumber = new String();
 let mode = "deg";
+let currentAction = undefined;
 
 /* initialize buttons */
 //initialize all of the numbers
@@ -23,6 +24,7 @@ const multiply = document.querySelector("#multiply");
 const subtract = document.querySelector("#subtract");
 const add = document.querySelector("#add");
 const equals = document.querySelector("#equals");
+const clear = document.querySelector('#clear');
 
 //buttons
 const deg = document.querySelector("#deg");
@@ -42,6 +44,13 @@ number7.onclick = () => {processNumber(7);}
 number8.onclick = () => {processNumber(8);}
 number9.onclick = () => {processNumber(9);}
 
+//basic math operations processing
+divide.onclick = () => {processAction('divide');}
+multiply.onclick = () => {processAction('multiply');}
+subtract.onclick = () => {processAction('subtract');}
+add.onclick = () => {processAction('add');}
+equals.onclick = completeAction;
+clear.onclick = clearAll;
 
 //process input from other buttons
 deg.onclick = toggleDeg;
@@ -52,6 +61,60 @@ function processNumber(number) {
     currentNumber += number.toString();
     console.log(currentNumber);
     updateDisplay();
+}
+
+function completeAction() {
+    if (currentAction !== undefined) {
+        let num1 = parseInt(currentNumber);
+        let num2 = parseInt(altNumber);
+        let answer;
+        
+        if (currentAction === 'add') {
+            answer = num1 + num2;
+        }
+        else if (currentAction === 'subtract') {
+            answer = num2 - num1;
+        }
+        else if (currentAction === 'multiply') {
+            answer = num1*num2;
+        }
+        else if (currentAction === 'divide') {
+            answer = num2 / num1;
+        }
+
+        currentAction = undefined;
+
+        outputString = answer;
+        currentNumber = new String();
+        currentNumber += answer;
+        altNumber = new String();
+        updateDisplay();
+    }
+}
+
+function processAction(action) {
+    if (currentAction == undefined) {
+        currentAction = action;
+
+        altNumber = currentNumber;
+        currentNumber = new String();
+
+        if (action == 'add') {
+            outputString += '+';
+        }
+        else if (action == "subtract") {
+            outputString += "-";
+        }
+        else if (action == "divide") {
+            outputString += "/";
+        }
+        else if (action == "multiply") {
+            outputString += "*";
+        }
+
+        updateDisplay();
+
+    }
 }
 
 function updateDisplay() {
@@ -68,4 +131,12 @@ function toggleRad() {
     mode = "rad";
     rad.classList.add("enabled");
     deg.classList.remove("enabled");
+}
+
+function clearAll() {
+    outputString = '';
+    currentNumber = new String();
+    altNumber = new String();
+    currentAction = undefined;
+    updateDisplay();
 }
